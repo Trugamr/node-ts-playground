1ae7950a720ece15d1dc8004eff5bc8ea6759cbf
Trugamr <trugamr@live.in>
2023-11-03 13:54:03 +0530

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  printWidth: 96,
+}
+
+export default config
